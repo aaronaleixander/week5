ccea3189ea1f000f1bfcba9f62826ac5370fb411
Aaron Utterback <Autterback3@mail.greenriver.edu>
2021-02-05 23:17:23 -0500

    added views, and initial setup

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+// turn on error reporting
+ini_set('display_error', 1);
+error_reporting(E_ALL);
+
+// require the autoload file
+require_once('vendor/autoload.php');
+
+// create an instance of the base class
+$f3 = Base::instance();
+
+// Define a default route
+$f3->route('GET /' , function(){
+    // fat free - taking the view page and rendering it in the browser
+    $view = new Template();
+    echo $view->render('/views/info .html');
+});
+
+// Run fat free
+$f3->run();
